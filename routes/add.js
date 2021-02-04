d3234e601c6a5074e689b751cf7d5aec706ad460
Nhat Nguyen <nmn008@ucsd.edu>
2021-02-04 00:16:30 -0800

    Update Lab 5

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,18 @@
 var data = require("../data.json");
 
-exports.addFriend = function(request, response) {    
+exports.addFriend = function(request, response) {   
 	// Your code goes here
- }
+	// console.log("Here");
+	var name = request.query.name;
+	var desc = request.query.description;
+	// console.log(name + " " + desc);
+	var img = "https://thispersondoesnotexist.com/image";
+	var newFriend = {
+		name : name,
+		description: desc,
+		imageURL: img
+	}
+	data.friends.push(newFriend);
+	// console.log(data);
+	response.redirect('/');
+}
